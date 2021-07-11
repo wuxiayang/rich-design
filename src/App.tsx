861209d8df756343e3082cf80e6b417f079c5c07
xiayang.wu <xiayang.wu@casstime.com>
2021-07-11 16:31:30 +0800

    完成button组件测试用例_20210711

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 // import './App.css';
-// import Button, { ButtonType, ButtonSize} from './components/Button/button';
-import Alert, { AlertType } from './components/Alert/alert';
+import Button, { ButtonType, ButtonSize} from './components/Button/button';
+// import Alert, { AlertType } from './components/Alert/alert';
 
 function App() {
-  var flag = false;
-  const closeAlert = () =>{
-    flag = true;
-    console.log('回调传递');
-  }
-  console.log('flag',flag);
+  // var flag = false;
+  // const closeAlert = () =>{
+  //   flag = true;
+  //   console.log('回调传递');
+  // }
+  // console.log('flag',flag);
   return (
     <div className="App">
       <header className="App-header">
-        {/* <Button 
+        <Button 
           onClick={(e)=>{e.preventDefault();console.log('111')}}
           btnType={ButtonType.Default} 
           size={ButtonSize.Large}
@@ -42,15 +42,15 @@ function App() {
           disabled={true}
         >
           Disabled
-        </Button> */}
-        <Alert 
+        </Button>
+        {/* <Alert 
           type={AlertType.Waring}
           title="提示标题"
           showIcon={true}
           message={<div>提示内容2</div>}
           closable={flag}
           onClose={()=>closeAlert()}
-        />
+        /> */}
       </header>
     </div>
   );
