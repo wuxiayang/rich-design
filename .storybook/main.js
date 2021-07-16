1a8a1f33de1cb731a3f8c362b4c83267caca712e
xiayang.wu <xiayang.wu@casstime.com>
2021-07-16 14:51:22 +0800

    20210716_添加STORYBOOK

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,29 @@
-const custom = require('./webpack.config.js');
-
 module.exports = {
-  webpackFinal: (config) => {
-    console.log('config: ', config);
-    return { ...config, module: { ...config.module, rules: custom.module.rules } };
-  },
   "stories": [
     "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.@(ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app"
-  ]
+  ],
+  webpackFinal: async (config) => {
+   config.module.rules.push({
+      test: /\.tsx?$/,
+      use: [
+        {
+          loader: require.resolve("babel-loader"),
+          options: {
+            presets: [require.resolve("babel-preset-react-app")]
+          }
+        },
+        require.resolve("react-docgen-typescript-loader")
+      ]
+    });
+  
+    config.resolve.extensions.push(".ts", ".tsx");
+  
+    return config;
+  }
 }
