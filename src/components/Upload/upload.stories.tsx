3fb48393bc33c86a6e5f57bfa4dda76f4ce473e2
xiayang.wu <xiayang.wu@casstime.com>
2021-08-25 18:05:27 +0800

    添加文件上传进度_20210825

--- src/components/Upload/upload.stories.tsx
+++ src/components/Upload/upload.stories.tsx
@@ -3,10 +3,29 @@ import { storiesOf } from "@storybook/react";
 import { action } from '@storybook/addon-actions';
 import { Upload } from "./upload";
 
+// 判断文件大小
+const checkFileSize = (file: File) => {
+    if(Math.round(file.size / 1024) > 50) {
+        alert('file too big');
+        return false;
+    }
+    return true;
+}
+
+const filePromise = (file: File) => {
+    const newFile = new File([file], 'new_name.docx', { type: file.type});
+    return Promise.resolve(newFile);
+}
+
 const SimpleUpload = () => {
     return (
         <Upload
             action="http://jsonplaceholder.typicode.com/posts/"
+            onChange={action('changed')}
+            // beforeUpload={filePromise}
+            // onProgress={action('progress')}
+            // onSuccess={action('success')}
+            // onError={action('error')}
         />
     )
 }
--- src/components/Upload/upload.tsx
+++ src/components/Upload/upload.tsx
@@ -1,23 +1,54 @@
-import React, { ChangeEvent, FC, useRef } from "react";
+import React, { ChangeEvent, FC, useRef, useState } from "react";
 import axios from "axios";
 
 import Button from "../Button/button";
 
+export type UploadFileStatus = 'ready' | 'uploading' | 'success' | 'error';
+
+export interface UploadFile {
+    uid: string;
+    size: number;
+    name: string;
+    status?: UploadFileStatus;
+    percent?: number;
+    raw?: File;
+    response?: any;
+    error?: any;
+}
+
+
+
 export interface UploadProps {
     action: string;
+    beforeUpload?: (file: File) => boolean | Promise<File>;
     onProgress?: (percentage: number, file: File) => void;
     onSuccess?: (data: any, file: File) => void;
     onError?: (err: any, file: File) => void;
+    onChange?: (file: File) => void;
 }
 
 export const Upload: FC<UploadProps> = (props) => {
     const {
         action,
+        beforeUpload,
         onProgress,
         onSuccess,
-        onError
+        onError,
+        onChange,
     } = props;
     const fileInput = useRef<HTMLInputElement>(null);
+    const [ fileList, setFileList ] = useState<UploadFile[]>([]);
+    const updateFileList = (uploadFile: UploadFile, updateObj: Partial<UploadFile>) => {
+        setFileList(prevList => {
+            return prevList.map(file => {
+                if(file.uid === uploadFile.uid) {
+                    return { ...file, ...updateObj }
+                } else {
+                    return file;
+                }
+            })
+        })
+    }
     const handleClick = () => {
         if(fileInput.current) {
             fileInput.current.click()
@@ -26,11 +57,80 @@ export const Upload: FC<UploadProps> = (props) => {
     const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
         const files = e.target.files;
         if(!files) return;
-        // uploadFiles(files);
+        uploadFiles(files);
+        if(fileInput.current){
+            fileInput.current.value = ''
+        }
     }
-    const uploadFiles = () => {
-        
+    const uploadFiles = (files: FileList) => {
+        let postFiles = Array.from(files);
+        postFiles.forEach(file => {
+            if(!beforeUpload) { 
+                post(file); 
+            } else {
+                const result = beforeUpload(file);
+                if(result && result instanceof Promise){
+                    result.then(processFile => {
+                        post(processFile);
+                    })
+                } else if (result !== false){
+                    post(file);
+                } 
+            } 
+        })
     }
+    const post = (file: File) => {
+        let _file: UploadFile = {
+            uid: Date.now() + 'upload-file',
+            status: 'ready',
+            name: file.name,
+            size: file.size,
+            percent: 0,
+            raw: file
+        };
+        setFileList([_file, ...fileList]);
+        const formData = new FormData();
+        formData.append(file.name, file);
+        axios.post(action, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            },
+            onUploadProgress: (e) => {
+                let percentage = Math.round((e.loaded * 100) / e.total) || 0;
+                if(percentage < 100) {
+                    // setFileList((prevList) => {
+                    //     console.log('prevList: ', prevList);
+                    //     return prevList;
+                    // })
+                    updateFileList(_file, { percent: percentage, status: 'uploading'})
+                    if(onProgress) {
+                        onProgress(percentage, file);
+                    }
+                }   
+            }
+        }).then(resp => {
+            console.log('resp: ', resp);
+            updateFileList(_file, {  status: 'success', response: resp.data });
+            if(onSuccess) {
+                onSuccess(resp.data, file)
+            }
+            if(onChange) {
+                onChange(file)
+            }
+        }).catch(err => {
+            console.error(err);
+            updateFileList(_file, { status: 'error', error: err})
+            if(onError) {
+                onError(err, file);
+            }
+            if(onChange) {
+                onChange(file)
+            }
+        })
+    }
+
+    console.log(fileList);
+
     return (
         <div
             className="rich-upload-component"
