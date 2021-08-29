324924b042f97239af4a3b0f7ec6127d125fc272
wuxiayang <2624745855@qq.com>
2021-08-29 17:23:58 +0800

    修改问题5_20210829

--- src/components/AutoComplete/autoComplete.stories.tsx
+++ src/components/AutoComplete/autoComplete.stories.tsx
@@ -3,10 +3,10 @@ import { storiesOf } from "@storybook/react";
 import { action } from '@storybook/addon-actions';
 import { AutoComplete, DataSourceType } from "./autoComplete";
 
-interface  LakerPlayerProps {
-    value: string;
-    number?: number;
-}
+// interface  LakerPlayerProps {
+//     value: string;
+//     number?: number;
+// }
 
 interface GithubUserProps {
     login?: string;
@@ -15,20 +15,20 @@ interface GithubUserProps {
 }
 
 const SimpleComplete = () => {
-    const  lakers = [
-        'bradley', 'pope', 'caruso', 'cock', 'cousins',
-        'james', 'AD','green'
-    ];
-    const  lakersWithNumber = [
-        { value: 'bradley', number: 0},
-        { value: 'pope', number: 1},
-        { value: 'caruso', number: 2},
-        { value: 'cock', number: 3},
-        { value: 'cousins', number: 4},
-        { value: 'james', number: 5},
-        { value: 'green', number: 6},
-        { value: 'AD', number: 7},
-    ];
+    // const  lakers = [
+    //     'bradley', 'pope', 'caruso', 'cock', 'cousins',
+    //     'james', 'AD','green'
+    // ];
+    // const  lakersWithNumber = [
+    //     { value: 'bradley', number: 0},
+    //     { value: 'pope', number: 1},
+    //     { value: 'caruso', number: 2},
+    //     { value: 'cock', number: 3},
+    //     { value: 'cousins', number: 4},
+    //     { value: 'james', number: 5},
+    //     { value: 'green', number: 6},
+    //     { value: 'AD', number: 7},
+    // ];
     const handleFecth = ( query: string) => {
         return fetch(`https://api.github.com/search/users?q=${query}`)
         .then(res => res.json())
@@ -57,14 +57,14 @@ const SimpleComplete = () => {
     //         </>
     //     ) 
     // }
-    const renderOption = (item: DataSourceType<GithubUserProps>) => {
-        return (
-            <>
-                <h2>Name: {item.login}</h2>       
-                <p>Url: {item.url}</p>
-            </>
-        ) 
-    }
+    // const renderOption = (item: DataSourceType<GithubUserProps>) => {
+    //     return (
+    //         <>
+    //             <h2>Name: {item.login}</h2>       
+    //             <p>Url: {item.url}</p>
+    //         </>
+    //     ) 
+    // }
 
     return (
         <AutoComplete
